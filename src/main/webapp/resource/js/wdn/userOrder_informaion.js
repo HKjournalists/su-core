0b2970e0c0bacad2058b50a8c2fca40b03e26cfb
Sam Zhou <samzhouzl@gmail.com>
2016-09-16 17:01:18 +0800

    优化我的文献订单

--- src/main/webapp/resource/js/wdn/userOrder_informaion.js
+++ src/main/webapp/resource/js/wdn/userOrder_informaion.js
@@ -3,40 +3,15 @@ $(function() {
 	var owdn=window.excel=window.wdn||{};
 	var wdnServiceBaseUrl = fsn.getHttpPrefix()+"/sampling/import/";
 
-	var orderInfo = new kendo.data.DataSource({
-		type : "JSON",
-		transport : {
-			read : {
-				url : fsn.HTTP_PREFIX + "/wdn/getOrdersInfo"
-			}
-		},
-		schema : {
-			data : function(response) {
-				if (response.results&&response.results.length>0) {
-					return response.results;
-				}
-				return [];
-			},
-			total : function(response) {
-				if (response.result) {
-					return response.result.length;
-				}
-				return 0;
-			}
-		},
-		serverPaging : true,
-		pageSize : 10
-	});
-
 	$("#grid").kendoGrid({
 		columns: [
-			{ field: "orderId", title: "订单号", width: 60},
-			{ field: "title", title: "文献标题", width: 100},
-			{ field: "authors", title: "文献作者", width: 100},
-			{ field: "dataSource", title: "文献来源", width: 100},
-			{ field: "status", title: "订单状态", width: 60},
+			{ field: "orderId", title: "订单号", width: "auto"},
+			{ field: "title", title: "文献标题", width: "auto"},
+			{ field: "authors", title: "文献作者", width: "auto"},
+			{ field: "dataSource", title: "文献来源", width: "auto"},
+			{ field: "status", title: "订单状态", width: "auto"},
 			{ field: "applyDate", title: "请求时间",
-				template:'#:new Date(applyDate).format("YYYY-MM-dd")#', width: 80},
+				template:'#:new Date(applyDate).format("YYYY-MM-dd")#', width: "auto"},
 			{ command: [{name:"review",
 				text:"查看",
 				click:function(e){
@@ -44,13 +19,34 @@ $(function() {
 					var	dataItem = this.dataItem($(e.currentTarget).closest("tr"));
 					window.sessionStorage.setItem("currentOrder", JSON.stringify(dataItem));
 					window.open( fsn.getContextPath() + "/views/wdn/viewOrderInfo.html", "_self");
-				}}], title: "操作", width: 30 }
+				}}], title: "操作", width: "auto" }
 		],
-		dataSource: orderInfo,
-		pageable: {
-			refresh: true,
-			messages: fsn.gridPageMessage(),
+		dataSource: {
+		    type : "JSON",
+            		transport : {
+            			read : {
+            				url : fsn.HTTP_PREFIX + "/wdn/getOrdersInfo"
+            			}
+            		},
+            		schema : {
+            			data : function(response) {
+            				if (response.results && response.results.length>0) {
+            					return response.results;
+            				}
+            				return [];
+            		    },
+            		    total : function(response) {
+                        				if (response.result) {
+                        					return response.result.length;
+                        				}
+                        				return 0;
+                        			}
+            		},
+            		pageSize : 10,
+                    serverPaging: true
 		},
+		pageSize : 10,
+		pageable: true,
 	});
 
 });
